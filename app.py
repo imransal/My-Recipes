c92bcd3a7b4644c4edfc162c4a10cac39006784e
imransal <>
2023-07-23 14:28:39 +0000

    Connected Flask to MongoDB and setup recipes.html page

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
-from flask_pymongo import flask_pymongo
+from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
