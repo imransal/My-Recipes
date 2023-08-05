a664b5f8901e602c6c341f45fff818642d55f70d
imransal <>
2023-08-05 09:08:14 +0000

    updated app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ def edit_recipe(recipe_id):
         mongo.db.recipes.update({"_id": ObjectId(recipe_id)}, submit)
         flash("Recipe Successfully Updated")
 
-    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe)})
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_recipe.html", recipe=recipe, categories=categories)
 
